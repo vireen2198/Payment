4c065e5278263ed8102c529b3e4d0d510d32d328
vireen2198 <109417472+vireen2198@users.noreply.github.com>
2022-12-29 12:12:18 +0800

    Update stripe.js

--- controllers/stripe.js
+++ controllers/stripe.js
@@ -6,7 +6,7 @@ const getPaymentLink=async (param)=>{
   try {
     const session=await stripe.checkout.sessions.create({
 
-      payment_method_types:['card'],
+      payment_method_types:['card',`grabpay`],
       billing_address_collection:`required` , 
       invoice_creation: {
         enabled: true,
